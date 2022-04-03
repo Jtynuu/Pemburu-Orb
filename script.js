088936423ac661e4cc194c662846ff85c028b3b2
Nuu Programmer <86546730+goose07@users.noreply.github.com>
2022-04-03 22:38:49 +0700

    Update - script.js

--- script.js
+++ script.js
@@ -1,36 +1,20 @@
 // Mini Games is mini program.
 function miniGames() {
-  const alpha = window.confirm("Apakah anda mau lihat lebih lanjut untuk proyeknya?");
+  const alpha = window.confirm("Apakah anda ingin bermain gamenya?");
 
   if (alpha) {
-    const prompt = window.prompt("Masukkan huruf tunggal. (A ~ E)");
+    const prompt = parseInt(window.prompt("Masukkan Angka! *bebas"));
+    let random = Math.floor(Math.random() * 10) + 1;
+    const result = random * prompt;
 
-    switch (prompt) {
-      case 'A':
-        console.info(Math.floor(Math.random() * 2) + 1 + " Poin telah didapatkan!");
-        window.alert("Buka Consolenya Sekarang!");
-        break;
-      case 'B':
-        console.info(Math.floor(Math.random() * 4) + 1 + " Poin telah didapatkan!");
-        window.alert("Buka Consolenya Sekarang!");
-        break;
-      case 'C':
-        console.info(Math.floor(Math.random() * 6) + 1 + " Poin telah didapatkan!");
-        window.alert("Buka Consolenya Sekarang!");
-        break;
-      case 'D':
-        console.info(Math.floor(Math.random() * 8) + 1 + " Poin telah didapatkan!");
-        window.alert("Buka Consolenya Sekarang!");
-        break;
-      case 'E':
-        console.info(Math.floor(Math.random() * 10) + 1 + " Poin telah didapatkan!");
-        window.alert("Buka Consolenya Sekarang!");
-        break;
-      default:
-        window.alert("Buka Consolenya Sekarang!");
-        console.info("Sepertinya anda belum memasukkan kata kunci A-E pada form, atau mungkin anda memasukkan kata kunci yang salah. Silahkan klik tombol \"Mulai\" lagi.");
-      };
+    if (prompt) {
+      window.alert("Buka Consolenya Sekarang!")
+      console.info(result + " Orbs telah didapatkan!");
     } else {
-      console.info("Anda telah menolak pengajakan game, anda sangat sus.");
-    };
+      window.alert("Buka Consolenya Sekarang!");
+      console.info("Sepertinya anda belum memasukkan angka pada sesi prompt.");
+    }
+  } else {
+    console.info("Anda telah menolak pengajakan game, anda sangat sus.");
   };
+};
