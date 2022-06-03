cd913a6020787de57916a3c4bd63514839c9456e
Jtynuu <86546730+Jtynuu@users.noreply.github.com>
2022-06-03 19:51:19 +0700

    Update - script.js

--- script.js
+++ script.js
@@ -1,29 +1,14 @@
+// Deklarasi Konten
+const satuan = ["ohm", "Ampere", "Volt", "Newton", "Joule", "Watt"];
+const kata = ["*Anda berburu di hutan liar dengan kekuatan ", "*Anda memancing di sungai dengan kekuatan ", "*Anda merampok bank dengan kekuatan ", "*Anda melakukan meditasi dengan kekuatan ", "*Anda makan buah duren dengan kekuatan "];
 
-// Dibawah adalah kode sistem dari Pemburu Orb
+// Function Utama
 function miniGames() {
-  const bahan = ["Bawang", "Seledri", "Apel", "Pare", "Jagung", "Kol", "Tomat"];
-  const satuan = ["ohm", "Ampere", "Volt", "Newton", "Joule", "Watt"];
-
-  if (true) {
-    const prompt = Math.floor(Math.random() * 100) + 1;
-    let random = Math.floor(Math.random() * 27) + 1;
-    let random2 = Math.floor(Math.random() * 13) + 1;
-    let random3 = Math.floor(Math.random() * 12) + 1;
-    const result = random * prompt;
-    const result2 = random2 * prompt;
-    const result3 = random3 * prompt;
-    const bahanAcak = bahan[Math.floor(Math.random() * bahan.length)];
-    const powerAcak = satuan[Math.floor(Math.random() * satuan.length)];
-
-    if (prompt) {
-      document.getElementById("resultIndex").innerHTML = result.toLocaleString() + " Orbs";
-      document.getElementById("result2Index").innerHTML = result2.toLocaleString() + ` ${bahanAcak}`;
-      document.getElementById("result3Index").innerHTML = result3.toLocaleString() + " Koin";
-      
-      document.getElementById("ket").innerHTML = `*Anda telah menggunakan power sebesar ${prompt} ${powerAcak}. Dan anda mendapatkan...`;
-      
-    } else {
-      window.alert("Err! Sepertinya anda belum memasukkan angka pada sesi form, atau ada unsur huruf ketika anda memasukkannya... Jika merasa ada yang salah segera hubungi pihak pengembang melalui twitter di https://twitter.com/goose07");
-    }
-  }
+  const rand = Math.floor(Math.random() * 100) + 1;
+  const random = Math.floor(Math.random() * 1) + 1;
+  const result = random * rand;
+  const jum = `${rand} ${satuan[Math.floor(Math.random() * satuan.length)]}. `;
+  
+  document.getElementById("ket").innerHTML = kata[Math.floor(Math.random() * kata.length)] + jum + "Dan anda mendapatkan...";
+  document.getElementById("resultIndex").innerHTML = result.toLocaleString() + " Orbs";
 }
